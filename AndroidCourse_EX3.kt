ec29b8a1daf52b2b4b6edd31c6075a0ec1947343
Aljawhara Almadhi <114245447+Aljawhara75@users.noreply.github.com>
2022-10-05 23:59:37 +0300

    Add files via upload

---
+++ AndroidCourse_EX3.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    var arrayDouble = arrayListOf(5.5,7.8,5.9,9.7 ,9.3)
+    var average: Double=0.0
+
+    for (index in 0 until arrayDouble.size){
+            average+=arrayDouble[index]
+    }
+println("The average is ${average/arrayDouble.size}")
+
+
+}
